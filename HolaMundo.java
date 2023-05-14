191f2935004a729114dfeff87d01cb3e296b5fbc
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-13 21:14:09 -0500

    Se imprime Bienvenida al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
         System.out.println("¡Hola mundo!");
+        System.out.println("Bienvenida al curso");
        
     }
 }
